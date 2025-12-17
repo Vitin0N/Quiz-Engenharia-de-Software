6a9fda087de0cae324a3b7d2cd5960f7283a8857
Victor G. Menezes <victormrdv2@gmail.com>
2025-12-17 17:59:00 -0300

    adicinando env

--- getQuestions.py
+++ getQuestions.py
@@ -1,13 +1,13 @@
 import pandas as pd
-import json
+import os
 
 def get_questions():
     '''
     Busca os registros das perguntas no banco de questões retirando as questões testes de Alan
     retorna uma lista de dicionários (formato padrão JSON) 
     '''
-    sheet_id = '1oreiAVDcHUE2ZlNI2wRquKoSsFjP_TL40cdIH8E3XHs'
-    sheet_gid = '51751594'
+    sheet_id = os.getenv("SHEET_ID")
+    sheet_gid = os.getenv("SHEET_GID")
 
     url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={sheet_gid}'
 
